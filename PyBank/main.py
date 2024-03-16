6de44bcf88ff1e2b6b2b40055662f2b410ddbdb4
Ethan Severance <ethanseverance02@gmail.com>
2024-03-16 15:20:42 -0400

    added average change and some notes

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,17 +7,10 @@ csv_path = (r"C:\Users\Sezy\onedrive\python-challenge\PyBank\Resources\budget_da
 # Variable Names
 total_months = 0
 
-
+Last_p_and_L = 0
 net_total_P_L = 0
-
+# help store data
 changes_in_PnL = []
-
-dates=[]
-
-
-
-
-
 
 
 
@@ -29,9 +22,25 @@ with open (csv_path) as csvfile:
     next(csvreader)
 
     for row in csvreader:
+
         Date = row[0]
         Profit_Loss = int(row[1])
         net_total_P_L += Profit_Loss
-        print(net_total_P_L)
+        # for each row in csv reader add 1 to total months
+        total_months += 1
+        # change in P and L
+        if Last_p_and_L != 0:
+            change = Profit_Loss - Last_p_and_L
+            changes_in_PnL.append(change)
+        Last_p_and_L = Profit_Loss
+# 85 because we are doing change so there are only 85 changes or len()
+Average_change = sum(changes_in_PnL)/ len(changes_in_PnL)
 
+
+
+        
+        
+print(net_total_P_L)
+print(total_months)
+print(Average_change)
 
