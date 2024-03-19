150c3b8f8a9513b9faa4787a717f9102d1f04daa
Ethan Severance <ethanseverance02@gmail.com>
2024-03-18 22:31:53 -0400

    pretty good

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,8 +10,7 @@ two_votes = 0
 three_votes = 0
 Votes_per_canidate = 0
 Total_votes = 0 
-ALL_Canidates = set() 
-last_canidate = ""
+ALL_Canidates = []
 Stockham_percentage = 0 
 DeGette_percentage = 0
 Doane_percentage = 0
@@ -28,35 +27,43 @@ with open(csv_path) as csvfile:
         Canidate = str(row[2])
         County = row[1]
         
-        if last_canidate != Canidate:
-            ALL_Canidates.add(Canidate)
+        if Canidate not in ALL_Canidates:
+            ALL_Canidates.append(Canidate)
 # First Canidate Data
-        if Canidate == "Charles Casper Stockham":
+        if Canidate == ALL_Canidates[0]:
             one_votes += 1 
+        elif Canidate == ALL_Canidates[1]:
+            two_votes += 1 
+        else:
+            three_votes += 1 
         
+        DeGette_percentage = (two_votes / Total_votes)*100
+        DeGette_percentage = "{:.3f}%".format(DeGette_percentage)
         Stockham_percentage = (one_votes / Total_votes)*100
         Stockham_percentage = "{:.3f}%".format(Stockham_percentage)
-
-        # if Canidate == "Diana DeGette":
+        Doane_percentage = (three_votes / Total_votes)*100
+        Doane_percentage = "{:.3f}%".format(Doane_percentage)
 
 
         
 Canidatelist = list(ALL_Canidates)
-
+winner = max(one_votes, two_votes, three_votes)
+if winner == one_votes:
+    YOUWON = ALL_Canidates[0]
+elif winner == two_votes:
+    YOUWON = ALL_Canidates[1]
+else:
+    winner = ALL_Canidates[2]
 
 
            
-
-
-
-
-
-print(Canidatelist[0])
-print(Total_votes)
-print(one_votes)
-print(list(ALL_Canidates)[0])
-print(Stockham_percentage)
-        
+print("Election Results")
+print("====================")
+print(f"{ALL_Canidates[0]}: {Stockham_percentage} ({one_votes})")
+print(f"{ALL_Canidates[1]}: {DeGette_percentage} ({two_votes})")
+print(f"{ALL_Canidates[2]}: {Doane_percentage} ({three_votes})")
+print("======================")
+print(f'Winner: {YOUWON}')
 
 
 
